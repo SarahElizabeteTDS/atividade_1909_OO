c81a1fed363294c460eca85d60f6044d41631467
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-09-19 14:34:04 -0300

    Update execucao.php

--- appTimes/execucao.php
+++ appTimes/execucao.php
@@ -2,8 +2,37 @@
 
 //requires
 require_once("modelo/Time.php");
+require_once("modelo/Jogador.php");
 //criar o objeto time
 $time = new Time();
 $time->setNome("Grêmio");
 $time->setCidade("Porto Alegre");
-print_r($time);
+//print_r($time);
+
+//criar o jogador
+$suarez = new Jogador();
+$suarez->setNome("Luiz Soarez");
+$suarez->setNumero(9);
+$suarez->setTime($time);
+
+//criar o jogador
+$geromel = new Jogador();
+$geromel->setNome("Pedro Geromel");
+$geromel->setNumero(4);
+$geromel->setTime($time);
+
+print_r($suarez);
+
+//imprimir o nome do time
+//print $suarez->getTime()->getNome();
+
+//so pra inicializar
+$jogadores = array();
+//add ao array
+array_push($jogadores, $suarez);
+array_push($jogadores, $geromel);
+
+$time->setJogadores($jogadores);
+print_r($time->getJogadores());
+//ou
+print_r($jogadores);
